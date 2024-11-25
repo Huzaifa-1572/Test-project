34ba073f58237c5d207864e8065140f5f18cc555
Muhammad Daniyal Saleem <daniyal.saleem@paysyslabs.com>
2024-11-25 17:47:34 +0500

    change config url

--- src/Utils/config.js
+++ src/Utils/config.js
@@ -1,8 +1,8 @@
 // LOCAL
-export const BASE_URL = "http://192.168.20.101:8080/";
+// export const BASE_URL = "http://192.168.20.101:8080/";
 
 // UAT
-// export const BASE_URL = "http://10.6.60.6:8089/";
+export const BASE_URL = "http://10.6.60.6:8089/";
 
 export const ENDPOINTS = {
   AUTHENTICATION: "api/dao/v1/authenticate",
